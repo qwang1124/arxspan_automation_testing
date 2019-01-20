c43079c3c7e334d941a2db9936687bf9ac023d83
bchen354 <bchen354@gatech.edu>
2019-01-20 18:13:32 -0500

    add automation script.

---
+++ login_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'start running login automation'
+python3 -m pytest testcases/login/login_test_admin.py --alluredir loginreport
+echo 'start generating the report'
+allure serve loginreport
