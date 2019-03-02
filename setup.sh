5288c96c8cc33e337aeb11aaa94cd52d99dd0ff2
bchen354 <bchen354@gatech.edu>
2019-03-02 17:52:50 -0500

    update setup command

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ echo "install pip3"
 sudo apt-get install python3-pip
 echo "install selenium"
 pip3 install selenium
+echo "install webdriver-manager"
+pip3 install webdriver-manager
+echo "install install pytest"
+pip3 install pytest
+echo "install allure pytest"
+pip3 install allure-pytest
