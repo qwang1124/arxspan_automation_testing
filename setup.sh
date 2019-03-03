beece87178fae4e8778a80db7c28ad649937dd8c
bchen354 <bchen354@gatech.edu>
2019-03-02 19:02:45 -0500

    add dummy xml file

--- setup.sh
+++ setup.sh
@@ -7,9 +7,12 @@ echo "install webdriver-manager"
 pip3 install webdriver-manager
 echo "install install pytest"
 pip3 install pytest
+pip install pytest-cov 
+pip install pytest-xdist 
+pip install pytest-bdd 
 echo "install allure pytest"
 pip3 install allure-pytest
-echo "install allure"
-sudo apt-add-repository ppa:qameta/allure
-sudo apt-get update 
-sudo apt-get install allure
+# echo "install allure"
+# sudo apt-add-repository ppa:qameta/allure
+# sudo apt-get update 
+# sudo apt-get install allure
