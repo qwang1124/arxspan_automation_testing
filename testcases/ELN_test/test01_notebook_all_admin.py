6394cd69d9073f7290189ab7a6ecf75ec5ccf47a
qwang1124 <qwang1124@gmail.com>
2019-03-15 17:01:07 -0400

    0308

--- testcases/ELN_test/test01_notebook_all_admin.py
+++ testcases/ELN_test/test01_notebook_all_admin.py
@@ -16,31 +16,68 @@
 #      8. Log out.
 import time
 import unittest
-from webdriver_manager.chrome import ChromeDriverManager
-import allure
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import platform
 import json
+from pathlib import Path
 
 
-class TestCreatenotebookAdmin(unittest.TestCase):
+class TestCreateexperimentJoe(unittest.TestCase):
+    def setUp(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-gpu')
+        if platform.system() == 'Windows':
+            self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+        elif platform.system() == "Darwin":
+            self.driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
+        else:
+            self.driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
 
-    @allure.testcase('test create notebook by Admin')
-    def test1(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get('https://model.arxspan.com/login.asp')
+        self.driver.implicitly_wait(3)
+        self.base_url = "https://model.arxspan.com/login.asp"
+
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_create_chemistryexperiment(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        driver.find_element_by_id('login-email').send_keys('joe@demo.com')
+        driver.find_element_by_id('login-pass').send_keys('carbonCopee')
+        driver.find_element_by_id('login-submit').send_keys(Keys.RETURN)
+        select = Select(driver.find_element_by_tag_name('select'))
+        select.select_by_visible_text('Model Test Script Company')
+        driver.find_element_by_id('login-submit').send_keys(Keys.ENTER)
+        cookies = driver.get_cookies()
+        print(type(cookies))
+        # print ("".join(cookies))
+        f1 = open('cookieadmin.txt', 'w')
+        f1.write(json.dumps(cookies))
+        f1.close
         f1 = open('cookieadmin.txt')
         cookie = f1.read()
         cookie = json.loads(cookie)
         for c in cookie:
             driver.add_cookie(c)
         driver.refresh()
-        # create notebook by admin
-        driver.find_element_by_id('createNewNotebookLeftNavButton').click()
-        driver.find_element_by_id('notebookName').send_keys('Test_Note_book_QW')
-        driver.find_element_by_name('notebookDescription').send_keys('Test Script execution-01/01/2019')
-        driver.find_element_by_name('createNotebook').click()
+        driver.implicitly_wait(20)
+        driver.get('https://model.arxspan.com/arxlab/dashboard.asp')
+
+        # Create new chemistry experiment
+        driver.find_element_by_xpath('//*[@id="navMyNotebooks"]/ul/li/a').click()
+        driver.find_element_by_css_selector(
+            '#pageContentTD > div > div.createExperimentDiv > a:nth-child(2)').click()
+        assert WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.ID, "historyNavLink")))
+        driver.close()
 
         test_value = driver.find_element_by_id('NotebookTitle').text
         print(test_value)
